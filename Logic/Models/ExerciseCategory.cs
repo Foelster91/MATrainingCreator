2bba63c330f61c1b8c5fd62e9f4e08f1a94541a3
Foelster91 <marcfoelster@gmx.de>
2016-12-02 12:06:52 +0100

    Added basic models for exercises, exercise categories and sessions in the logic layer

---
+++ Logic/Models/ExerciseCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class ExerciseCategory
+    {
+        #region Properties
+
+        //Every ExerciseCategory has a name
+        public String Name { get; set; }
+
+        //An ExerciseCategory might have an discription
+        public String Description { get; set; }
+
+        //ExerciseCategories should be arranged in a hierarchy
+        //e. g. Karate (Supercategory) -> Kata (Subcategory)
+        public ExerciseCategory SuperCategory { get; set; }
+
+        #endregion Properties
+    }
+}
---
+++ Logic/Models/TrainingSession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class TrainingSession
+    {
+        #region Properties
+
+        //Every training session has a name
+        public String Name { get; set; }
+
+        //Every training session should have a short description with goal specification
+        public String Description { get; set; }
+
+        //A training session might be created for a specific date
+        public DateTime Date { get; set; }
+
+        //Every training session has a time measured in minutes
+        public int Time { get; set; }
+
+        //Every training session has a set of exercises
+        public ObservableCollection<Exercise> Exercises { get; set; }
+
+        #endregion Properties
+    }
+}
