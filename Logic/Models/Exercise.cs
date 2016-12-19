56e2de6500e63f65421d0c95da776dd3fba1427d
Foelster91 <marcfoelster@gmx.de>
2016-12-19 18:48:37 +0100

    Created basic UI elements in the ExerciseAdministrationView

--- Logic/Models/Exercise.cs
+++ Logic/Models/Exercise.cs
@@ -13,16 +13,17 @@ namespace Logic.Models
         //Exercises should have a name
         public String Name { get; set; }
 
-        //Exercises have a description
-        public String Description { get; set; }
-
         //An exercise belongs to an exercise category.
         public ExerciseCategory ExerciseCategory { get; set; }
+
+        //Exercises have a description
+        public String Description { get; set; }
 
         //An exercise has a time interval in minutes in which it has to be completed
         public int Time { get; set; }
 
         //An exercise can be countable or not: e. g. Push-ups are countable, Running is not
+        // 40 oder 50 should not be different exercises
         public bool IsCountable { get; set; }
 
         //An exercise can have an amount of sets, independant from the property iscountable
