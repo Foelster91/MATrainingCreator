148595d0caab4267f2f31bb65a11dde87b628183
Foelster91 <marcfoelster@gmx.de>
2016-12-27 15:49:56 +0100

    Added testing folder and project

---
+++ GUITests/MainWindowViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GUITests
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
